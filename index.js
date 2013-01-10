b6ad0278c280d87cbf294495ef1de38f17b8dff1
Rod Vagg <rod@vagg.org>
2013-01-10 11:18:41 +1100

    return to using path+fd keys

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
 const fs = require('fs')
 
-var cleanupFd    = function (path) {
+var totalOpenFds = 0 // across all instances
+
+    // the reason we use a combination of path+fd to store references
+    // in this._usingfd is that it is possible to have multiple fds
+    // for the same file simultaneously. Particularly in the situation
+    // where an fd is pending for a close but hasn't been checked back
+    // in by all clients
+  , key          = function (path, fd) {
+      return fd + ':' + path
+    }
+
+  , cleanupFd    = function (path, fd) {
       // clear the use counter & close if pending
-      if (this._pendingClose[path]) {
-        fs.close(this._pendingClose[path], function () {})
-        module.exports._totalOpenFds--
-        delete this._pendingClose[path]
+
+      delete this._usingfd[key(path, fd)]
+      delete this._usingfd[path]
+
+      if (this._pendingClose[key(path, fd)]) {
+        console.log('close', path, fd)
+        fs.close(fd, function () {})
+        totalOpenFds--
+        delete this._pendingClose[key(path, fd)]
       }
     }
 
-  , close       = function (path) {
+  , close       = function (path, fd) {
       // dispose of this fd when possible
-      var fdr = this._fds[path]
-      if (!fdr) return
 
-      this._pendingClose[path] = fdr.fd
+      this._pendingClose[key(path, fd)] = fd
       // nextTick needed to match the nextTick in an async-cache otherwise we may
       // close it in the tick prior to it being actually needed
       process.nextTick(function () {
-        if (fdr.co === 0)
-          cleanupFd.call(this, path)
+        if (!this._usingfd[key(path, fd)])
+          cleanupFd.call(this, path, fd)
       }.bind(this))
     }
 
@@ -28,55 +42,61 @@ var cleanupFd    = function (path) {
 
       // if the file is already open and in use but not with a close pending
       // then just use that.
-      if (this._fds[path] && !this._pendingClose[path])
-        return cb(null, this._fds[path].fd)
+      if (this._usingfd[path] && !this._pendingClose[key(path, this._usingfd[path])])
+        return cb(null, this._usingfd[path])
 
       fs.open(path, 'r', function (er, fd) {
         if (!er) {
-          module.exports._totalOpenFds++
-          this._fds[path] = {
-              fd : fd
-            , co : 0
-          }
+        console.log('open', path, fd)
+          totalOpenFds++
+          this._usingfd[path] = fd
+          this._usingfd[fd + path] = 0
         }
 
         cb(er, fd)
       }.bind(this))
     }
 
-  , checkout     = function (path) {
+  , checkout     = function (path, fd) {
       // call whenever you *may* be about to use the fd, to ensure it's not cleaned up
 
-      var fdr = this._fds[path]
-      if (!fdr) throw new Error('no fd for path: ' + path)
-
-      fdr.co++
-      return fdr.fd
+      this._usingfd[path] = fd
+      this._usingfd[key(path, fd)] = (this._usingfd[key(path, fd)] || 0) + 1
     }
 
-  , checkin     = function (path) {
+  , checkin     = function (path, fd) {
       // call sometime after checkout() when you know you're not using it
 
-      var fdr = this._fds[path]
-      if (!fdr) throw new Error('no fd for path: ' + path)
+      if (this._usingfd[path] && --this._usingfd[key(path, fd)] === 0)
+        cleanupFd.call(this, path, fd)
+    }
 
-      if ((fdr.co = Math.max(fdr.co - 1, 0)) === 0)
-        cleanupFd.call(this, path)
+  , checkinfn   = function (path, fd) {
+      // make a checkin function that can be safely called multiple times
+
+      var called = false
+      return function () {
+        if (!called) {
+          this.checkin(path, fd)
+          called = true
+        }
+      }.bind(this)
     }
 
   , FDManager   = {
-        open     : open
-      , close    : close
-      , checkout : checkout
-      , checkin  : checkin
+        open      : open
+      , close     : close
+      , checkout  : checkout
+      , checkin   : checkin
+      , checkinfn : checkinfn
     }
 
   , create      = function () {
       return Object.create(FDManager, {
-          _fds      : { value: Object.create(null) }
+          _usingfd      : { value: Object.create(null) }
         , _pendingClose : { value: Object.create(null) }
       })
     }
 
 module.exports               = create
-module.exports._totalOpenFds = 0
+module.exports._totalOpenFds = totalOpenFds
